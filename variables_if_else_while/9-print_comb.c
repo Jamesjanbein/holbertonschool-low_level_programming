260863f5b39aea399ba8b0f99a85effbae3dac81
ahmadjanbein <jamesjanbein@gmail.com>
2023-10-21 12:35:23 -0700

    a

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : all s digi nbs
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit =0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48)'
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
