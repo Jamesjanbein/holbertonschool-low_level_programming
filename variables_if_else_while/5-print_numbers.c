a1671ff3a4e418e4890e14ff043c1caf447776ff
ahmadjanbein <jamesjanbein@gmail.com>
2023-10-21 11:41:11 -0700

    a

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : all s digi nbs
+ *
+ * Return: always 0 
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
