0c34cd049f4c1bc809e2290b4fd0bff4884f411d
ahmadjanbein <jamesjanbein@gmail.com>
2023-10-21 12:21:42 -0700

    a

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+  
+/**
+ * main - entry point
+ *
+ * Description : all s digi nbs
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102) 
+	{
+		putchar(digit);
+
+		if (digit ==57)
+			digit += 39;
+		diit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
