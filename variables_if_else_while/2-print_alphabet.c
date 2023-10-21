1a6c00563c1cc72a12653ba9755854b975b45fdd
ahmadjanbein <jamesjanbein@gmail.com>
2023-10-21 10:40:11 -0700

    a

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print a-z
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
