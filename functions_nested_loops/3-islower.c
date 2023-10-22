67adbb58378ea7fc357b285fe0b8a094fc922049
ahmadjanbein <jamesjanbein@gmail.com>
2023-10-21 23:14:42 -0700

    b

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,8 +5,7 @@
  *
  * @c: input
  *
- * return: always 0 (Success)
-           otherwise returns 1 if 'c' is lowercase
+ * return: always 0 (Success) , otherwise returns 1 if 'c' is lowercase
 */
 
 int _islower(int c)
